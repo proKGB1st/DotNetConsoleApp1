9cddf0cd4a4a787182970b550d9f41bff434a3da
proKGB1st <133960645+proKGB1st@users.noreply.github.com>
2023-06-26 20:39:40 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DotNetConsoleApp1
+{
+    class Program
+    {
+        public static string Task = "\n";
+        
+        public static uint PublicMethod(uint m, uint n)
+        {
+            return 0;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine(Task);
+            
+            uint m = 0;
+
+            try
+            {
+                Console.Write("Input M value: ");
+                m = Convert.ToUInt32(Console.ReadLine());
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Format error occorupted!\n\n");
+            }
+        }
+    }
+}
