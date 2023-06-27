41346e9ca2c486a31996195bfd5f01bb40a6e18e
Alex <Alex@I21013401822.istu.int>
2023-06-27 15:06:54 +0800

    Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным

--- Program.cs
+++ Program.cs
@@ -2,70 +2,32 @@
 
 namespace DotNetConsoleApp1
 {
-    class MainApp
+    class Program
     {
-        private Int64 input_number;
-        private Int64 digit_size = 0;
-        private uint digit_value;
-
-        public MainApp(string? input)
+        public static void Main(string[] args)
         {
+            System.Console.Clear();
+            System.Console.WriteLine("Input your number (from 1 to 7):");
+
+            byte mask = 0x7C;
+
             try
             {
-                this.input_number = System.Int64.Parse(input == null ? "" : input);
+                Int16 input_value = System.Int16.Parse(System.Console.ReadLine());
 
-                if (this.input_number != this.input_number%100)
+                if (input_value > 7 || input_value < 1)
                 {
-                    this.GetInputDigit(this.ReverseNumber(this.input_number));
+                    System.Console.WriteLine("Format error ocorrupted.");
+                    return;
                 }
-
-                else
-                {
-                    System.Console.WriteLine("Input number {0} has an incorrect format (needed at least to be more or equal 3 digits)", this.input_number);
-                }
+                
+                System.Console.WriteLine("Is holiday? {0}", (((mask >> (input_value - 1)) & mask) != 0x0 ? "False" : "True"));
             }
 
             catch(System.FormatException ex)
             {
                 System.Console.WriteLine("Format error ocorrupted.");
             }
-        }
-
-        private Int64 ReverseNumber(Int64 num)
-        {
-            Int64 reversed = 0;
-            while (num > 0)
-            {
-                reversed *= 10;
-                reversed += num % 10;
-                num /= 10;
-            }
-            return reversed;
-        }
-
-        private void GetInputDigit(Int64 num)
-        {
-            if ((num /= 10) > 0)
-            {
-                if (2 == ++this.digit_size)
-                {       
-                    System.Console.WriteLine("The third digit value {0}", num%10);
-                    return;
-                }
-
-                GetInputDigit(num);
-            }
-        }
-    }
-
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            System.Console.Clear();
-            System.Console.WriteLine("Input your number (anyway digits format/no zero beginning):");
-            new MainApp(System.Console.ReadLine());
-            System.Console.WriteLine();
         }
     }
 }
