f8b240574f628602977696254030dcfa5f4f2151
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-06 17:22:52 +0800

    Пограмма, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве

--- Program.cs
+++ Program.cs
@@ -4,86 +4,49 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        private Int64 input_number;
-        private Int64 reversed_number;
-        private Int64 digit_size = 0;
+        public static string Task = "Задача 21. Напишите программу, \nкоторая принимает на вход координаты двух точек \nи находит расстояние между ними в 3D пространстве.\n";
 
-        public MainApp(string? input)
-        {
-            System.Console.Clear();
-            System.Console.WriteLine("Input your number (from 1 to 7):");
-
-            byte mask = 0x7C;
-
-            try
-            {
-                Int16 input_value = System.Int16.Parse(System.Console.ReadLine());
-
-                this.GetInputDigit(this.input_number);
-                
-                if (5 <= this.digit_size)
-                {
-                    this.ReverseNumber(this.input_number);
-
-                    System.Console.WriteLine(
-                        "Input number{0} palindrome!", 
-                        this.reversed_number == this.input_number ? "'s" : " isn't "
-                    );
-                }
-                
-                else
-                {
-                    System.Console.WriteLine(
-                        "Input number {0} has an incorrect format (needed at least to be more or equal 5 digits)", 
-                        this.input_number
-                    );
-                }
-            }
-
-            catch(System.FormatException ex)
-            {
-                System.Console.WriteLine("Format error ocorrupted.");
-            }
-        }
-
-        private void ReverseNumber(Int64 num)
-        {
-            this.reversed_number = 0;
-            
-            while (num > 0)
-            {
-                this.reversed_number *= 10;
-                this.reversed_number += num % 10;
-                num /= 10;
-            }
-        }
-
-        private void GetInputDigit(Int64 num)
-        {
-            if ((num /= 10) > 0)
-            {
-                ++this.digit_size;
-                GetInputDigit(num);
-            }
-
-            else if (this.input_number % 10 > 0)
-            {
-                ++this.digit_size;
-            }
-        }
-    }
-
-    class Program
-    {
-        public static string Task = "Напишите программу, \nкоторая принимает на вход пятизначное число и проверяет, \nявляется ли оно палиндромом.\n";
-        
         public static void Main(string[] args)
         {
             System.Console.Clear();
             System.Console.WriteLine(Task);
-            System.Console.WriteLine("Input your number (anyway digits format/no zero beginning):");
-            new MainApp(System.Console.ReadLine());
-            System.Console.WriteLine();
+            
+            int count = 0;
+            int point = 0;
+
+            int[,] Points = new int[2,3];
+
+            while ((bool) (count != -1))
+            {
+                try
+                {
+                    System.Console.WriteLine("Input your {0} number for your {1} point", count+1, point+1);
+                    Points[point, count] = (int) System.Int64.Parse(System.Console.ReadLine());
+
+                    if (++count > 2)
+                    {
+                        count = 0;
+                        if ((bool) (++point > 1))
+                        {
+                            count = -1;
+                        }
+                    }
+                }
+
+                catch(System.FormatException ex)
+                {
+                    System.Console.WriteLine("Format error ocorrupted.");
+                }
+            }
+
+            // Заранее можно было бы посчитать в цикле, если выводить запрос на числа
+            // для первой и второй точки последовательно, а не по порядку..
+            
+            int rx = Points[1,0] - Points[0,0];
+            int ry = Points[1,1] - Points[0,1];
+            int rz = Points[1,2] - Points[0,2];
+
+            System.Console.WriteLine("The distance between two vectors is: {0}", Math.Sqrt(Math.Pow(rx, 2) + Math.Pow(ry, 2) + Math.Pow(rz, 2)));
         }
     }
 }
