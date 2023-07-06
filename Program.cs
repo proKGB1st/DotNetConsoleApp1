a09999b8270ffa5666516c69b6a72a0dcf3002a3
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-06 17:50:51 +0800

    Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

--- Program.cs
+++ Program.cs
@@ -4,49 +4,35 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 21. Напишите программу, \nкоторая принимает на вход координаты двух точек \nи находит расстояние между ними в 3D пространстве.\n";
+        public static string Task = "Задача 23. Напишите программу,\n которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.\n";
 
         public static void Main(string[] args)
         {
             System.Console.Clear();
             System.Console.WriteLine(Task);
             
-            int count = 0;
-            int point = 0;
+            int num = 0;
 
-            int[,] Points = new int[2,3];
-
-            while ((bool) (count != -1))
+            try
             {
-                try
-                {
-                    System.Console.WriteLine("Input your {0} number for your {1} point", count+1, point+1);
-                    Points[point, count] = (int) System.Int64.Parse(System.Console.ReadLine());
-
-                    if (++count > 2)
-                    {
-                        count = 0;
-                        if ((bool) (++point > 1))
-                        {
-                            count = -1;
-                        }
-                    }
-                }
-
-                catch(System.FormatException ex)
-                {
-                    System.Console.WriteLine("Format error ocorrupted.");
-                }
+                System.Console.WriteLine("Input your number:");
+                num = (int) System.Int64.Parse(System.Console.ReadLine());
             }
 
-            // Заранее можно было бы посчитать в цикле, если выводить запрос на числа
-            // для первой и второй точки последовательно, а не по порядку..
-            
-            int rx = Points[1,0] - Points[0,0];
-            int ry = Points[1,1] - Points[0,1];
-            int rz = Points[1,2] - Points[0,2];
+            catch(System.FormatException ex)
+            {
+                System.Console.WriteLine("Format error ocorrupted.");
+            }
 
-            System.Console.WriteLine("The distance between two vectors is: {0}", Math.Sqrt(Math.Pow(rx, 2) + Math.Pow(ry, 2) + Math.Pow(rz, 2)));
+            System.Console.Write("\nYour number's: {0} -> ", num);
+
+            int count = 0;
+
+            while ((bool) (count != num))
+            {
+                int val = (int) Math.Pow(++count, 3);
+                System.Console.Write("{0}{1}", (count != 1 ? ", " : ""), val);
+            }
         }
     }
 }
