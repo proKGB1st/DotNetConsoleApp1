74df98d33bdf1dfb09e304a408daced18c5c675f
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-07 12:04:38 +0800

    Массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве

--- Program.cs
+++ Program.cs
@@ -4,19 +4,33 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.\n";
+        public static string Task = "Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.\n";
 
         public static void Main(string[] args)
         {
             System.Console.Clear();
             System.Console.WriteLine(Task);
+
+            Random rg = new Random();
             
-            int[] arr = new int[8];
+            int[] arr = new int[rg.Next(0, 64)];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rg.Next(0, 999);
+            }
+
+            int counter = 0;
 
             foreach (int val in arr)
             {
-                System.Console.Write("{0}", val);
+                if (0 == val%2)
+                {
+                    counter++;
+                }
             }
+
+            System.Console.Write("{0} ", counter);
         }
     }
 }
