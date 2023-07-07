32ddcbb4ac539b7ef402985d8d5e123661112268
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-07 11:55:38 +0800

    Программа, которая задаёт массив из 8 элементов и выводит их на экран

--- Program.cs
+++ Program.cs
@@ -4,35 +4,19 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.\n";
+        public static string Task = "Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.\n";
 
         public static void Main(string[] args)
         {
             System.Console.Clear();
             System.Console.WriteLine(Task);
             
-            int num = 0;
+            int[] arr = new int[8];
 
-            try
+            foreach (int val in arr)
             {
-                System.Console.WriteLine("Input your number:");
-                num = (int) System.Int64.Parse(System.Console.ReadLine());
+                System.Console.Write("{0}", val);
             }
-
-            catch(System.FormatException ex)
-            {
-                System.Console.WriteLine("Format error ocorrupted.");
-            }
-
-            int sum = 0;
-
-            while(num > 0)
-            {
-                sum += num % 10;
-                num /= 10;
-            }
-
-            System.Console.WriteLine("Value: {0}", sum);
         }
     }
 }
