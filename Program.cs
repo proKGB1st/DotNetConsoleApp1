77179f6ae88332be1ee474c1e2759c5a75662fee
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-11 15:56:22 +0800

    Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями

--- Program.cs
+++ Program.cs
@@ -4,31 +4,43 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.\n";
-        
-        static float NextFloat(Random rand)
-        {
-            double val = rand.NextDouble();
-            val -= 0.5;
-            val *= 2;
-            return float.MaxValue * (float) val;
-        }
-        
+        public static string Task = "Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями.\n";
+
         public static void Main(string[] args)
         {
             System.Console.Clear();
             System.Console.WriteLine(Task);
 
-            Random rg = new Random();
-            
-            double[] arr = new double[rg.Next(0, 64)];
-
-            for (int i = 0; i < arr.Length; i++)
+            try
             {
-                arr[i] = NextFloat(rg);
+                System.Console.WriteLine("Input k1:");
+                Int64 k1 = 0;
+                k1 = System.Int64.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("Input b1:");
+                Int64 b1 = 0;
+                b1 = System.Int64.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("Input k2:");
+                Int64 k2 = 0;
+                k2 = System.Int64.Parse(System.Console.ReadLine());
+
+                System.Console.WriteLine("Input b2:");
+                Int64 b2 = 0;
+                b2 = System.Int64.Parse(System.Console.ReadLine());
+                
+                Int64 k = k1-k2;
+                Int64 x = k/(b1-b2);
+                Int64 y = k2*x+b2;
+
+                System.Console.WriteLine("Cross point: ({0};{1})", x, y);
             }
 
-            System.Console.Write("Min: {0}, Max: {1} => {2}", arr.Min(), arr.Max(), (arr.Max() - arr.Min()));
+            catch(System.FormatException ex)
+            {
+                System.Console.WriteLine("Format error ocorrupted.");
+            }
+
         }
     }
 }
