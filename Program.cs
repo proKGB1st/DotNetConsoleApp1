ed2eb39e2842c9fe15cc7cc4dd34439daba1b023
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-17 08:37:04 +0800

    Программа, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 47.\n Задайте двумерный массив размером m×n,\n заполненный случайными вещественными числами\n";
+        public static string Task = "Задача 50.\n Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,\n и возвращает значение этого элемента или же указание, что такого элемента нет\n";
 
         public static void Main(string[] args)
         {
@@ -13,19 +13,19 @@ namespace DotNetConsoleApp1
 
             try
             {
-                Console.Write("Input M: ");
+                Console.Write("Pos M: ");
                 uint m = Convert.ToUInt32(Console.ReadLine());
 
-                Console.Write("Input N: ");
+                Console.Write("Pos N: ");
                 uint n = Convert.ToUInt32(Console.ReadLine());
 
-                uint[,] a = new uint[m,n];
+                uint[,] a = new uint[3,4];
 
                 Random r = new Random();
 
-                for (uint x = 0; x < m; x++)
+                for (uint x = 0; x < 3; x++)
                 {
-                    for (uint y = 0; y < n; y++)
+                    for (uint y = 0; y < 4; y++)
                     {
                         a[x, y] = (uint) (r.Next(Int32.MinValue, Int32.MaxValue) + (uint)Int32.MaxValue);
 
@@ -34,13 +34,28 @@ namespace DotNetConsoleApp1
 
                     Console.WriteLine("\n");
                 }
+
+                try
+                {
+                    Console.WriteLine("Found index value in pos {0},{1}: {0}", m, n, a[m, n]);
+                }
+
+                catch (IndexOutOfRangeException)
+                {
+
+                    throw;
+                }
             }
 
-            catch (System.FormatException ex)
+            catch (FormatException)
             {
                 Console.WriteLine("Format error occorupted!\n\n");
             }
 
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("No such index in array!\n\n");
+            }
         }
     }
 }
