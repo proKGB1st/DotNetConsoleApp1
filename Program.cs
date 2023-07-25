ff112132c92b8d62ac835727631303ab9f2a1aa4
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-25 15:49:34 +0800

    Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 56: \n\nЗадайте прямоугольный двумерный массив. \nНапишите программу, которая будет находить строку с наименьшей суммой элементов.\n";
+        public static string Task = "Задача 58: \n\nЗадайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.\n";
+
+        // метод для умножения матриц
+        static uint[,] GetMatrix(uint num)
+        {
+            uint m = num;
+            uint n = num;
+
+            uint[,] a = new uint[m, n];
+
+            Random r = new Random();
+
+            Console.Write("\n== Input =====================\n\n");
+
+            for (uint x = 0; x < m; x++)
+            {
+                for (uint y = 0; y < n; y++)
+                {
+                    a[x, y] = (uint)r.Next(0, 8);
+                    
+                    Console.WriteLine("\tМассив a[{0},{1}] = {2}", x, y, a[x, y]);
+                }
+
+                Console.Write("\n");
+            }
+
+            return a;
+        }
 
         public static void Main(string[] args)
         {
@@ -13,60 +40,45 @@ namespace DotNetConsoleApp1
 
             try
             {
-                Console.Write("Input M: ");
-                uint m = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Input matrix size: ");
 
-                Console.Write("Input N: ");
-                uint n = Convert.ToUInt32(Console.ReadLine());
+                uint size = Convert.ToUInt32(Console.ReadLine());
 
-                uint[,] a = new uint[m,n];
+                uint[,] a = GetMatrix(size);
+                uint[,] b = GetMatrix(size);
 
-                Random r = new Random();
+                uint[,] r = new uint[size, size];
 
-                Console.Write("\n== Input =====================\n\n");
-
-                for (uint x = 0; x < m; x++)
+                for (var i = 0; i < size; i++)
                 {
-                    for (uint y = 0; y < n; y++)
+                    for (var j = 0; j < size; j++)
                     {
-                        a[x, y] = (uint) r.Next(0, 23); 
-                        // a[x, y] = (uint) (r.Next(Int32.MinValue, Int32.MaxValue) + (uint) Int32.MaxValue);
+                        r[i, j] = 0;
 
-                        Console.WriteLine("\tМассив a[{0},{1}] = {2}", x, y, a[x, y]);
+                        for (var k = 0; k < size; k++)
+                        {
+                            r[i, j] += a[i, k] * b[k, j];
+                        }
+                    }
+                }
+
+                Console.Write("\n== Print =====================\n\n");
+
+                for (uint x = 0; x < size; x++)
+                {
+                    for (uint y = 0; y < size; y++)
+                    {
+                        Console.WriteLine("\tМассив r[{0},{1}] = {2}", x, y, r[x, y]);
                     }
 
                     Console.Write("\n");
                 }
-
-                Console.WriteLine("== Sorting =====================\n");
-
-                uint idx = 0;
-                uint min = 0;
-
-                for (uint x = 0; x < m; x++)
-                {
-                    uint sum = 0;
-
-                    for (uint y = 0; y < n; y++)
-                    {
-                        sum += a[x, y];
-                    }
-
-                    if ((min == 0) || min > sum)
-                    {
-                        min = sum;
-                        idx = x;
-                    }
-                }
-
-                Console.WriteLine("\tСтрока {0} с найменьшей суммой {1}: ", idx, min);
             }
 
-            catch (System.FormatException ex)
+            catch (FormatException ex)
             {
                 Console.WriteLine("Format error occorupted!\n\n");
             }
-
         }
     }
 }
