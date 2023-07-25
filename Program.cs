ffbc1ad16ae41d6c4fd0d803e68006c0b8b5d236
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-25 15:13:41 +0800

    Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 54: Задайте двумерный массив. \nНапишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.\n";
+        public static string Task = "Задача 56: \n\nЗадайте прямоугольный двумерный массив. \nНапишите программу, которая будет находить строку с наименьшей суммой элементов.\n";
 
         public static void Main(string[] args)
         {
@@ -29,7 +29,7 @@ namespace DotNetConsoleApp1
                 {
                     for (uint y = 0; y < n; y++)
                     {
-                        a[x, y] = (uint) r.Next(0, 23);
+                        a[x, y] = (uint) r.Next(0, 23); 
                         // a[x, y] = (uint) (r.Next(Int32.MinValue, Int32.MaxValue) + (uint) Int32.MaxValue);
 
                         Console.WriteLine("\tМассив a[{0},{1}] = {2}", x, y, a[x, y]);
@@ -40,34 +40,26 @@ namespace DotNetConsoleApp1
 
                 Console.WriteLine("== Sorting =====================\n");
 
-                for (uint x = 0; x < m; x++)
-                {
-                    uint temp;
-
-                    for (uint y = 0; y < n; y++)
-                    {
-                        for (uint z = 0; z < n; z++)
-                        {
-                            if (a[x,y] > a[x,z])
-                            {
-                                temp = a[x,y];
-                                a[x,y] = a[x,z];
-                                a[x,z] = temp;
-                            }
-                        }
-                    }
-                }
+                uint idx = 0;
+                uint min = 0;
 
                 for (uint x = 0; x < m; x++)
                 {
+                    uint sum = 0;
+
                     for (uint y = 0; y < n; y++)
                     {
-
-                        Console.WriteLine("\tМассив a[{0},{1}] = {2}", x, y, a[x, y]);
+                        sum += a[x, y];
                     }
 
-                    Console.Write("\n");
+                    if ((min == 0) || min > sum)
+                    {
+                        min = sum;
+                        idx = x;
+                    }
                 }
+
+                Console.WriteLine("\tСтрока {0} с найменьшей суммой {1}: ", idx, min);
             }
 
             catch (System.FormatException ex)
