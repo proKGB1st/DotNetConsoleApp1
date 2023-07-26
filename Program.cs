5a9a01adc493223b296d9a450bfd077a60f9a385
Alex Deroza Torettovich <pro@kgb1st.ru>
2023-07-26 12:03:39 +0800

    Задача 66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N

--- Program.cs
+++ Program.cs
@@ -4,28 +4,45 @@ namespace DotNetConsoleApp1
 {
     class Program
     {
-        public static string Task = "Задача 64. Задайте значение N.\nНапишите программу, которая выведет все натуральные числа в промежутке от N до 1.\nВыполнить с помощью рекурсии.\n";
+        public static string Task = "Задача 66.\nЗадайте значения M и N.\nНапишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.\n";
 
-        public static uint PrintNumbers(uint val)
+        public static uint PrintNumbers(uint c, uint m, uint n)
         {
-            if (val >= 1)
+            if (n > c)
             {
-                Console.Write($"{val--} ");
-                PrintNumbers(val);
+                m += n--;
+                m = PrintNumbers(c, m, n);
             }
 
-            return 0;
+            return m;
         }
 
         public static void Main(string[] args)
         {
             Console.Clear();
             Console.WriteLine(Task);
+            
+            uint m = 0;
+            uint n = 0;
 
             try
             {
-                Console.Write("Input value (recommended more than 1): ");
-                PrintNumbers(Convert.ToUInt32(Console.ReadLine()));
+                Console.Write("Input M value: ");
+                m = Convert.ToUInt32(Console.ReadLine());
+
+                Console.Write("Input N value: ");
+                n = Convert.ToUInt32(Console.ReadLine());
+
+                if (m > n)
+                {
+                    uint t = m;
+
+                    n = m;
+                    m = t;
+                }
+
+                uint sum = PrintNumbers(m, m, n);
+                Console.Write($"The numbers in: {m} and {n}. Sum's: {sum}");
             }
 
             catch (FormatException ex)
